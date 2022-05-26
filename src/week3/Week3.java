2d625df7c597101485fe95e62c316e2795555f3d
sin11889 <sin11889@sheridancollege.ca>
2022-05-26 12:47:38 -0400

    version 2

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -10,6 +10,7 @@ public class Week3 {
 
     /**
      * @param args the command line arguments
+     * week3
      */
     public static void main(String[] args) {
         System.out.println("Learning Github");
